eddae22e2bde816e810e886a1881460d868785eb
RAJA VIGNESH P <93259031+Raja-12377@users.noreply.github.com>
2024-06-02 22:15:09 +0530

    Add files via upload

---
+++ Sentiment.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pickle
+import time
+
+# load the model
+model = pickle.load(open('sentiment_model.pkl', 'rb'))
+
+st.title('Sentiment Analysis :) ')
+
+tweet = st.text_input('Enter your Comments or Tweet')
+
+submit = st.button('Predict')
+
+if submit:
+    start = time.time()
+    prediction = model.predict([tweet])
+    end = time.time()
+    st.write('Prediction time taken: ', round(end-start, 2), 'seconds')
+    
+    #print(prediction[0])
+    # st.write(prediction[0])
+    if prediction[0] == 'Positive':
+        st.success('Positive Comment  :) ')
+    elif prediction[0] == 'Negative':
+        st.warning('Negative Comment    :( ')
+    elif prediction[0] == 'Neutral':
+        st.info('Neutral Comment  :| ')
+
